4bd2a4a78d8cb3c182f41c0e68ea26ecfa12255c
Noah-Hsu <noahhsu@njust.edu.cn>
2024-03-27 13:59:08 +0800

    正确显示图片

--- DatasetTest.py
+++ DatasetTest.py
@@ -1,6 +1,5 @@
 from torch.utils.data import DataLoader
 
-import dataset
 from dataset import *
 
 # Normalization parameters for pre-trained PyTorch models
@@ -16,11 +15,18 @@ test_dataset = Dataset(dataset_path="../../0Dataset/UCF101/UCF-101-frames",
                        sequence_length=40,
                        training=False,
                        )
+
 test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=True, num_workers=0)
 for batch_index, (input_image, input_label) in enumerate(test_dataloader):
-    input_image, input_label = input_image.to(device), input_label.to(device)
+    input_image = input_image[0]  # 取出批次中的第一个样本
+    # input_image = input_image.to(torch.float32)  # 将数据类型转换为浮点型
     to_pil = transforms.ToPILImage()
-    for i in range(40):
-        image = dataset.denormalize(input_image[0][i].cpu())
-        pil_image = to_pil(image.byte())
-        pil_image.show()
+    # 执行逆归一化操作
+    for i in range(40):  # 遍历每个图像序列中的每一帧
+        for j in range(3):  # 遍历每个通道
+            input_image[i, j] = (input_image[i, j] * std[j]) + mean[j]
+
+    # 将张量转换为 PIL 图像并显示
+    for i in range(40):  # 遍历每个图像序列中的每一帧
+        pil_image = to_pil(input_image[i])  # 转换为字节张量并转换为 PIL 图像
+        pil_image.show()  # 显示图像
